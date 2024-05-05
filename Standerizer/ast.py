d1599000dbfa26a29cbd90266bd3527d9bdcf1fd
Irash Perera <irash.21@cse.mrt.ac.lk>
2024-05-05 12:42:15 +0530

    added explainability

--- Standerizer/ast.py
+++ Standerizer/ast.py
@@ -9,15 +9,18 @@ class AST:
         return self.root
 
     def standardize(self):
+        # Check if the root node is already standardized
         if not self.root.is_standardized:
+            # If not, call the standardize method on the root node
             self.root.standardize()
 
     def pre_order_traverse(self, node, i):
+        # Print the node's data with indentation based on the level
         print("." * i + str(node.get_data()))
+        # Traverse through each child node recursively
         for child in node.children:
             self.pre_order_traverse(child, i + 1)
 
     def print_ast(self):
+        # Start the pre-order traversal from the root node with initial indentation level 0
         self.pre_order_traverse(self.get_root(), 0)
-        
-
